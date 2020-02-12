9f0be0ebd202eeb38d3200f0b3a0959fce87b17f
Tejas Sheth <sheth.t.j.2012@gmail.com>
2020-02-12 22:54:00 +0530

    calculator service initial/blank project created

---
+++ calculator-service/src/test/java/com/example/calculator/calculatorservice/CalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator.calculatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
