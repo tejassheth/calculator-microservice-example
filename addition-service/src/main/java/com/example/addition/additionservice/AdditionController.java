57b2b9621fe7fd9a639b6399ed17247503d9028c
Tejas Sheth <sheth.t.j.2012@gmail.com>
2020-02-13 00:05:49 +0530

    Create RestController and added add method and set port and application name

---
+++ addition-service/src/main/java/com/example/addition/additionservice/AdditionController.java
@@ -0,0 +1,22 @@
+package com.example.addition.additionservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdditionController {
+    /**
+     *
+     * @param number1
+     * @param number2
+     * @return double
+     * This method is listen on /add url with two numbers and it will return
+     * sum of two numbers
+     *
+     */
+    @PostMapping("/add/{number1}/{number2}")
+    public double add(@PathVariable double number1,@PathVariable double number2){
+        return Double.sum(number1,number2);
+    }
+}
